54d07dd7423da09f1f9dad81f60f1bb5e34e9048
Lesli Chavez Contreras <lchavezcontreras1@my.wctc.edu>
2021-02-10 13:52:56 -0600

    Fixed it so that the text popped up but I am still having problems with the color

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ $(document).ready(
     });
 function greeting(e) {
     e.preventDefault();
-    let rank = ($("input[name=rank]").val()).data("color");
+    let rank = $("input[name=rank]:checked").val();
+    /*rank.each(function(){
+        $(this).css("color", $(this).data("color"));
+    });*/
     let firstName = $("input#firstname").val();
     let lastName= $("input#lastname").val();
     $("span#greeting").text(`Hello, ${rank} ${firstName} ${lastName}.`);
 }
+
